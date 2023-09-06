606ab093b778653808b0eaadafaef1a8b936f2d5
Rayhan Hossen <105411261+rayhan-hossen@users.noreply.github.com>
2023-09-06 22:51:17 +0600

    Add files via upload

---
+++ Day12.py
@@ -0,0 +1,16 @@
+#names = "rayhan, zakaria"
+#print(names [0:6])
+#print(len(names))
+
+fruit = "Mango"
+
+
+len1= len(fruit)
+print("Mango is a", len1, "letter word")
+
+print(fruit[1:3])
+print(fruit[:-3])
+
+nam = "harry"
+
+print(nam[-4:-2])
---
+++ Day13.py
@@ -0,0 +1,38 @@
+# Strings are immutable
+a = "Ha rr y"
+print(len(a))
+print(a.upper())
+print(a.lower())
+
+b = "Rayhan!!!!!!!!!"
+
+print(b.rstrip("!"))
+
+
+print(a.replace("Harry", "Nayan"))
+
+print(a.split(" "))
+
+Cap= "hello its time to capitalize"
+
+print(Cap.capitalize())
+
+cen="Welcome to 100 days of python"
+
+print(cen.center(50))
+
+print(cen.center(50, "-"))
+
+print(cen.count("o"))
+
+print(cen.endswith("n"))
+print(cen.endswith("."))
+
+print(cen.find("to"))
+
+print(cen.isalnum())
+print(cen.isalpha())
+
+print(cen.swapcase())
+
+print(cen.title())
