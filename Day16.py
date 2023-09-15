44cde9bd3f7d8f611470a49143a0c3cc7334d23b
Rayhan Hossen <105411261+rayhan-hossen@users.noreply.github.com>
2023-09-16 00:28:33 +0600

    Add files via upload

---
+++ Day16.py
@@ -0,0 +1,18 @@
+# name = 'Rayhan'
+
+# for i in name:
+#     print(i)
+#     if(i=="y"):
+#       print("This is something special")
+
+# colours = ["Red", "Green", "Blue"]
+
+# for i in colours:
+#     print(i)
+#     for colour in i:
+#         print(colour)
+#     if (i=="Red"):
+#         print("Its my favourite colour")
+
+for i in range(0, 2001 , 5):
+    print(i )
