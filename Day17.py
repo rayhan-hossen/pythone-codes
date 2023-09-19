da960550db94815f64f549e545e80f52e2094d03
Rayhan Hossen <105411261+rayhan-hossen@users.noreply.github.com>
2023-09-19 22:38:03 +0600

    Add files via upload

---
+++ Day17.py
@@ -0,0 +1,13 @@
+i=int(input("Enter the ammount : "))
+while(1<=38):
+    i=int(input("Enter the ammount : "))
+    print(i)
+
+print("Done the loop")
+    
+count=5
+while (count>0):
+    print(count)
+    count = count - 1
+else:
+    print('I am inside else')
---
+++ Day18.py
@@ -0,0 +1,17 @@
+# for i in range(12):
+#     print("5 X", i+1 , "=", 5 * (i+1))
+#     if (i==10):
+#         break
+# print("Loop sesh")
+
+# for i in range(12):
+#     if (i==10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X", i , "=", 5 * i)
+i=0
+while True:
+    print(i)
+    i= i+1
+    if(i%101==0):
+        break
