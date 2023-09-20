dea5b61bd7427ad8fc8bded0dc7c943c5e25e3c6
Rayhan Hossen <105411261+rayhan-hossen@users.noreply.github.com>
2023-09-20 23:13:12 +0600

    Add files via upload

---
+++ Day19.py
@@ -0,0 +1,40 @@
+def calculateGmean(a,b):
+    mean = (a*b)/ (a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+def isLessser(a,b):
+    pass #This will give me an opportunity to write the function body latter and it would not show any Error
+
+
+a= 9
+b =8
+isGreater(a,b)
+# if(a>b):
+#     print("First number is greater")
+# else:
+#     print("Second number is greater or equal")
+
+
+calculateGmean(a,b)
+# gmean = (a*b)/ (a+b)
+# print(gmean)
+
+c= 8
+d= 7
+isGreater(c,d)
+# if(c>d):
+#     print("First number is greater")
+# else:
+#     print("Second number is greater or equal")
+
+
+calculateGmean(c,d)
+
+# gmean2= (c*d)/(c+d)
+# print(gmean2)
