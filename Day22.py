17a90ce873be18f60771b4a948920c5d9fa7d43f
Rayhan Hossen <105411261+rayhan-hossen@users.noreply.github.com>
2023-10-25 18:44:25 +0600

    Add files via upload

---
+++ Day22.py
@@ -0,0 +1,14 @@
+l= [1,2,4,5]
+print(l)
+# l.append(6)
+# l.sort(reverse=True)
+# l.reverse()
+m=l.copy()
+m[0]=0
+print(l)
+
+a=['ray', 'han', 'tara']
+b=['nay', 'yan', 'tara']
+ #extend method
+a.extend(b)
+print(a)
---
+++ day23.py
@@ -0,0 +1,11 @@
+tup=(1,2,3,5,6,4,8,10)
+print( tup)
+print( tup[0])
+print( tup[1])
+print( tup[2])
+
+if 3 in tup:
+    print("yes there is 3")
+
+tup2= tup[1:5]
+print(tup2)
